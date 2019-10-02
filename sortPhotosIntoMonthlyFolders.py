a16ba0c13156f63c05b286dc46b13389e641099f
philcapon <phil.capon@fastmail.co.uk>
2019-10-02 08:23:56 +0100

    update

--- sortPhotosIntoMonthlyFolders.py
+++ sortPhotosIntoMonthlyFolders.py
@@ -25,7 +25,10 @@ def copy(filename, root_target, folder):
     target = target / folder[1]
     target.mkdir(exist_ok=True)
 
+    if (target / filename.name).exists():
+        return 1
     copy2(filename, target)
+    return 0
 
 
 # find datetimeoriginal tag
@@ -35,10 +38,12 @@ for k, v in ExifTags.TAGS.items():
     if v == 'DateTime':
         date_time_tag = k
 
-root_source = 'F:/2012'
+#root_source = 'F:/2012'
+root_source = 'temp/source'
 root_target = 'temp/target'
 
 num_copied = 0
+num_not_copied = 0
 
 # for each file in the folder
 for filename in Path(root_source).glob('**/*.*'):
@@ -59,9 +64,12 @@ for filename in Path(root_source).glob('**/*.*'):
 
     # create the folder if not present
     # copy the file
-    copy(filename, root_target, folder)
-
-    print('copied ' + filename.name + ' to ' + folder[1])
-    num_copied += 1
+    if copy(filename, root_target, folder) > 0:
+        print('DID NOT COPY ' + filename.name + ' to ' + folder[1])
+        num_not_copied += 1
+    else:
+        print('copied ' + filename.name + ' to ' + folder[1])
+        num_copied += 1
 
 print('Total photos copied: ', num_copied)
+print('Total photos NOT copied: ', num_not_copied)
